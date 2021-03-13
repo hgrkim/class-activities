48e4c3bf946a134d39c10a715b810395dbeb4b18
hgrkim <70608215+hgrkim@users.noreply.github.com>
2021-03-13 14:00:27 -0500

    19.1

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,10 @@
 // Create a variable below
 
+var helloworld = "Hello I'm Grace Kim. Welcome to my world!";
+
+
+
 // Console.log() your variable here.
+console.log(helloworld)
+
+alert(helloworld);
--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,25 @@
 // Create the addNumbers function here.
 
+var kidsinhouse = 3
+var kidsoutside = 2
+
+function totalkids(){
+alert('There are ' + kidsinhouse + kidsoutside + ' kids in total');
+}
+
+totalkids();
+
+
+
+
 // Create the stringConcat function here.
+
+var x = '3';
+var y = '4';
+var z = 'The sum is ' + x + y + ' right?';
+
+function string(){
+    alert(z)
+}
+
+string();
